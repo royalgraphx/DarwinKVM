9f143eafcf0f06b266d1fa5abbdf6016fd2d8b34
RoyalGraphX <royalgraphx@hypervisor.rgx.live>
2023-06-17 20:30:14 -0500

    Add DKVM OpenCore Submodule

---
+++ OpenCore/mount.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if nbd module is already loaded
+if lsmod | grep -q "^nbd"; then
+    echo "nbd module is already loaded. Proceeding..."
+else
+    echo "nbd module is not loaded. Loading..."
+    sudo modprobe nbd max_part=8
+fi
+
+# Check if /dev/nbd0 is already connected
+if lsblk -o NAME | grep -q "^nbd0$"; then
+    echo "/dev/nbd0 is already connected. Disconnecting..."
+    sudo qemu-nbd --disconnect /dev/nbd0
+fi
+
+sudo qemu-nbd --connect=/dev/nbd0 -f raw OpenCore.img
+echo "Connected /dev/nbd0."
+
+# Create "mnt" directory if it doesn't exist
+if [ ! -d "mnt" ]; then
+    mkdir mnt
+    echo "Created 'mnt' directory."
+fi
+
+# Mount /dev/nbd0 to the "mnt" directory with desired ownership
+sudo mount -o uid=$(id -u),gid=$(id -g) /dev/nbd0 mnt
+echo "Image mounted to 'mnt' directory with desired ownership successfully."
